1d0befedf60894370ebc683760b48089592b6715
xianlany <40433521+xianlany@users.noreply.github.com>
2022-01-27 15:49:34 -0500

    Up to slide 19

--- Lab 1/Lab1Try2.R
+++ Lab 1/Lab1Try2.R
@@ -24,4 +24,49 @@ qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
 qqline(x)
 
 #DALY
+summary(data$DALY) 	# stats
+fivenum(data$DALY,na.rm=TRUE)
+help(stem)
+stem(data$DALY)		 # stem and leaf plot
+help(hist)
+hist(data$DALY)
+hist(data$DALY, seq(30., 95., 1.0), prob=TRUE)
+help(lines)
+lines(density(data$DALY,na.rm=TRUE,bw=1.)) # or try bw=‚???oSJ‚???ù
+help(rug)
+rug(data$DALY)
 
+plot(ecdf(data$DALY), do.points=FALSE, verticals=TRUE)
+par(pty="s")
+qqnorm(data$DALY); qqline(data$DALY)
+
+#Make a Q-Q plot against the generating distribution by: 
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
+qqline(x)
+
+#WATER_H
+summary(data$WATER_H) 	# stats
+fivenum(data$WATER_H,na.rm=TRUE)
+help(stem)
+stem(data$WATER_H)		 # stem and leaf plot
+help(hist)
+hist(data$WATER_H)
+hist(data$WATER_H, seq(30., 95., 1.0), prob=TRUE)
+help(lines)
+lines(density(data$WATER_H,na.rm=TRUE,bw=1.)) # or try bw=‚???oSJ‚???ù
+help(rug)
+rug(data$WATER_H)
+
+plot(ecdf(data$WATER_H), do.points=FALSE, verticals=TRUE)
+par(pty="s")
+qqnorm(data$WATER_H); qqline(data$WATER_H)
+
+#Make a Q-Q plot against the generating distribution by: 
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
+qqline(x)
+
+
+boxplot(data$EPI,data$DALY) 
+qqplot(data$EPI,data$DALY)
