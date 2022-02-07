9e145314560a9372ffb901be64c02d460e51ae04
xianlany <40433521+xianlany@users.noreply.github.com>
2022-02-07 12:12:41 -0500

    finished lab 1 part 2

--- Lab 1/Lab1Part2.R
+++ Lab 1/Lab1Part2.R
@@ -72,3 +72,65 @@ abline(mm)
 abline(mm,col=3,lwd=3) # line color = green, line width = 3
 attributes(mm)
 mm$coefficients
+
+#creating plots
+plot(mtcars$wt,mtcars$mpg)
+library(ggplot2)
+qplot(mtcars$wt,mtcars$mpg)
+qplot(wt,mpg,data = mtcars)
+ggplot(mtcars,aes(x=wt,y=mpg)) + geom_point()
+plot(pressure$temperature, pressure$pressure, type = "l")
+points(pressure$temperature,pressure$pressure)
+
+lines(pressure$temperature, pressure$pressure/2, col="red")
+points(pressure$temperature, pressure$pressure/2, col="blue")
+library(ggplot2)
+qplot(pressure$temperature, pressure$pressure, geom="line")
+qplot(temperature, pressure, data = pressure, geom = "line")
+ggplot(pressure, aes(x=temperature,y=pressure)) + geom_line() + geom_point()
+ggplot(pressure, aes(x=temperature,y=pressure)) + geom_line() + geom_point()
+
+
+#Creating bar graphs
+barplot(BOD$demand, names.arg = BOD$Time)
+table(mtcars$cyl)
+barplot(table(mtcars$cyl))#generate a table of counts
+qplot(mtcars$cyl)
+qplot(factor(mtcars$cyl)) #treat cyl as discrete
+
+#bar graph of counts
+qplot(factor(cyl), data = mtcars)
+ggplot(mtcars, aes(x=factor(cyl)))+geom_bar()
+
+
+#creating histograms using ggplot
+hist(mtcars$mpg)
+hist(mtcars$mpg, breaks = 10) #specify approximate number of bins with breaks
+hist(mtcars$mpg, breaks = 5)
+hist(mtcars$mpg, breaks = 12)
+qplot(mpg, data=mtcars, binwidth=4)
+ggplot(mtcars,aes(x=mpg))+geom_histogram(binwidth = 4)
+ggplot(mtcars,aes(x=mpg))+geom_histogram(binwidth = 5)
+
+
+#creating box plots using ggplot
+plot(ToothGrowth$supp, ToothGrowth$len) #using plot() function and pass it as a factor of x values and a vector of y values
+#formula syntax
+boxplot(len ~ supp, data = ToothGrowth) #if the two vectors are in the same dataframe, you can use the formula syntax
+#with this syntax you can combine two variables on the x axis
+#put interaction of two variables on the x axis
+boxplot(len~supp + dose, data = ToothGrowth)
+#with ggplot you can get the same results above
+library(ggplot2)
+qplot(ToothGrowth$supp, ToothGrowth$len, geom = "boxplot")
+#if the two vectors are in the same dataframe, you can use the following syntax
+qplot(supp, len, data=ToothGrowth, geom= "boxplot")
+#in ggplot2, the above is equivalent to:
+ggplto(ToothGrowth, aes(x=supp, y=len)) + geom_boxplot()
+#using 3 separate vectors
+qplot(interaction(ToothGrowth$supp, ToothGrowth$dose), ToothGrowth$len, geom= "boxplot")
+#you can write the same thing above, get the columns from the dataframe
+qplot(interaction(supp, dose), len, data = ToothGrowth, geom = "boxplot")
+#using ggplot you can do the same thing and it is:
+ggplot(ToothGrowth, aes(x=interaction(supp, dose), y=len)) + geom_boxplot()
+#plotting a function curve
